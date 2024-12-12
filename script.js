9c1d25297d72a2e64bedd82dcd7578d002853c13
athu <athulyas2024@gmail.com>
2024-12-12 22:36:58 +0530

    quote generator website completed

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-// let button = document.getElementById("btn")
+document.getElementById('btn').addEventListener('click', async () => {
+    const response = await fetch('https://dummyjson.com/quotes');
+    const data = await response.json();
+    
+    if (data && data.quotes && data.quotes.length > 0) {
+      const randomIndex = Math.floor(Math.random() * data.quotes.length);
+      const randomQuote = data.quotes[randomIndex];
 
-async function fetchQuotes(){
-    let data = await fetch('https://dummyjson.com/quotes');
-    let response = await data.json()
-    console.log(response)
-}
-
-fetchQuotes().then(value=>{
-    // console.log(value)
-})
+      document.getElementById('quote').textContent = `"${randomQuote.quote}"`;
+      document.getElementById('author').textContent = `- ${randomQuote.author}`;
+    }
+});
